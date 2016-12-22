7cb6deee3941cab5c2fa8dd8bcf3ac2996856bb1
usuario <usuario@pc20-004>
2016-12-22 20:34:19 +0100

    modificado mensaje !!!!

--- src/com/toy/anagrams/ui/Anagrams.java
+++ src/com/toy/anagrams/ui/Anagrams.java
@@ -261,10 +261,10 @@ public class Anagrams extends JFrame {
 
     private void guessedWordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_guessedWordActionPerformed
         if (wordLibrary.isCorrect(wordIdx, guessedWord.getText())){
-            feedbackLabel.setText("Correct! Try a new word!");
+            feedbackLabel.setText("Correct! Try a new word!!!!!!");
             getRootPane().setDefaultButton(nextTrial);
         } else {
-            feedbackLabel.setText("Incorrecto! Intentalo de nuevo!");
+            feedbackLabel.setText("Incorrecto! Intentalo de nuevo!!!!!");
             guessedWord.setText("");
         }
 
